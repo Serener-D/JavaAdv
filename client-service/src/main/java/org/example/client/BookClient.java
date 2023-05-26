970cd00fc01010ebad82a0b84e1e3a327c2f1412
Denis Pronin <de.pronin@gmail.com>
2023-05-26 18:04:28 +0600

    added circuitbreaker for client-service

--- client-service/src/main/java/org/example/client/BookClient.java
+++ client-service/src/main/java/org/example/client/BookClient.java
@@ -1,14 +1,42 @@
 package org.example.client;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.model.Book;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "bookClient", url = "http://localhost:8890")
+@FeignClient(
+        name = "bookClient",
+        url = "http://localhost:8890",
+        fallbackFactory = BookClient.BookServiceConnectorFallbackFactory.class)
 public interface BookClient {
 
     @GetMapping("/api/books")
     List<Book> getAllBooks();
+
+    @Component
+    class BookServiceConnectorFallbackFactory implements FallbackFactory<FallbackWithFactory> {
+        @Override
+        public FallbackWithFactory create(Throwable cause) {
+            return new FallbackWithFactory(cause.getMessage());
+        }
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    class FallbackWithFactory implements BookClient {
+        private final String reason;
+
+        @Override
+        public List<Book> getAllBooks() {
+            log.info("Failed send request on book service, reason {}", reason);
+            return Collections.emptyList();
+        }
+    }
 }
