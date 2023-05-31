44a35f5864a0458be175a13860f5f5fc2aeade21
Denis Pronin <de.pronin@gmail.com>
2023-05-31 12:11:21 +0600

    updated CreationBookServiceApplication

--- creation-book-service/src/main/java/org/example/CreationBookServiceApplication.java
+++ creation-book-service/src/main/java/org/example/CreationBookServiceApplication.java
@@ -2,29 +2,29 @@ package org.example;
 
 import lombok.SneakyThrows;
 import org.example.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.Message;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableBinding(Source.class)
 public class CreationBookServiceApplication {
+
+    @Autowired
+    private Source source;
+
     public static void main(String[] args) {
         SpringApplication.run(CreationBookServiceApplication.class, args);
     }
 
-    @InboundChannelAdapter(channel = Source.OUTPUT, poller = @Poller(fixedRate = "5000"))
-    public Message<Book> generate(Book book) {
+    private void sendMessage(Book book) {
         System.out.println("Sending: " + book);
-        return MessageBuilder.withPayload(book)
-                .build();
+        source.output().send(MessageBuilder.withPayload(book).build());
     }
 
     @PostConstruct
@@ -32,7 +32,7 @@ public class CreationBookServiceApplication {
     private void init() {
         long counter = 0;
         while (true) {
-            generate(Book.builder().id(counter++).build());
+            sendMessage(Book.builder().id(counter++).build());
             Thread.sleep(10000);
         }
     }
