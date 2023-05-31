9dcb32195c30aa674e2423a513e4b4613602e4c2
Denis Pronin <de.pronin@gmail.com>
2023-05-31 21:25:25 +0600

    functional style for creation-book-service and check-book-service

--- check-book-service/src/main/java/org/example/CheckBookServiceApplication.java
+++ check-book-service/src/main/java/org/example/CheckBookServiceApplication.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import org.example.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class CheckBookServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheckBookServiceApplication.class, args);
+    }
+
+    @Bean
+    public Consumer<Book> bookConsumer() {
+        return book -> System.out.println("Received book: " + book);
     }
 }
--- creation-book-service/src/main/java/org/example/CreationBookServiceApplication.java
+++ creation-book-service/src/main/java/org/example/CreationBookServiceApplication.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import org.example.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class CreationBookServiceApplication {
+
+    private Long counter = 0L;
+
+    @Bean
+    public Supplier<Book> bookProducer() {
+        return () -> Book.builder().id(counter++).status(Book.Status.UNCHECKED).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CreationBookServiceApplication.class, args);
